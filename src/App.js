f469060383b71e6b4d4fdfcd19a8cba8e6e1fedb
Ali Kouchekzade <akmg1660@gmail.com>
2023-07-23 10:31:40 +0330

    develop App componenet

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>HELlllo</h1>
+      <p>this is paragraph</p>
+    </div>
+  );
+};
+
+/*const App = () => {
+    return React.createElement(
+      "div",
+      { id: "title", className: "app-title" },
+      "this is my first app"
+    );
+  };*/
+
+export default App;
