dc1b1a9ae742bdd7c7c9e8e0ca8a3c0df0bf6976
Ali Kouchekzade <akmg1660@gmail.com>
2023-07-23 11:41:20 +0330

    convert App To Class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import React, { Component } from "react";
 import Product from "./components/product/Product";
 
-const App = () => {
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>HELlllo</h1>
+        <Product name="js" price="10" />
+        <Product name="htm" price="20">
+          <p>discount is 15%</p>
+        </Product>
+        <Product name="css" price="30" />
+      </div>
+    );
+  }
+}
+
+/*const App = () => {
   return (
     <div className="container">
       <h1>HELlllo</h1>
@@ -11,7 +27,7 @@ const App = () => {
       <Product name="css" price="30" />
     </div>
   );
-};
+};*/
 
 /*const App = () => {
     return React.createElement(
