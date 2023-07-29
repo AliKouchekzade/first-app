a7c6fcd075dbe6928d02946ae0ffb9205d7dc513
Ali Kouchekzade <akmg1660@gmail.com>
2023-07-29 09:44:09 +0330

    useReducer -finetemplate !!!important

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -6,8 +6,8 @@ import { useProductsAction } from "../providers/productsProvider";
 const Product = ({ product, preTitle }) => {
   const inputRef = useRef();
 
-  const { deleteHandler, increaseHandler, decreseHandler, setTextHandler } =
-    useProductsAction();
+  //const { deleteHandler, increaseHandler, decreseHandler, setTextHandler } =useProductsAction();
+  const dispatchProducts = useProductsAction();
 
   useEffect(() => {
     inputRef.current.focus();
@@ -15,23 +15,39 @@ const Product = ({ product, preTitle }) => {
 
   return (
     <div className={style.product}>
-      <p>product : {product.title} + {preTitle}</p>
+      <p>
+        product : {product.title} + {preTitle}
+      </p>
       <p>price : {product.price} $</p>
       <span>{product.quantity} </span>
       <input
         ref={inputRef}
         type="text"
         value={product.title}
-        onChange={(event) => setTextHandler(event, product.id)}
+        onChange={(event) =>
+          dispatchProducts({ type: "setText", id: product.id, event: event })
+        }
       ></input>
-      <button onClick={() => increaseHandler(product.id)}>+</button>
+      <button
+        onClick={() =>
+          dispatchProducts({ type: "add", value: 1, id: product.id })
+        }
+      >
+        +
+      </button>
       <button
         className={`${product.quantity === 1 && style.remove}`}
-        onClick={() => decreseHandler(product.id)}
+        onClick={() =>
+          dispatchProducts({ type: "add", value: -1, id: product.id })
+        }
       >
         {product.quantity > 1 ? "-" : <FaTrashCan />}
       </button>
-      <button onClick={() => deleteHandler(product.id)}>delete</button>
+      <button
+        onClick={() => dispatchProducts({ type: "delete", id: product.id })}
+      >
+        delete
+      </button>
     </div>
   );
 };
--- src/components/providers/productsProvider.js
+++ src/components/providers/productsProvider.js
@@ -1,19 +1,28 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const productsContext = createContext();
 const productsContextDispatcher = createContext();
 
 const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([
+  /*const [products, setProducts] = useState([
     { title: "js", price: "99", id: 1, quantity: 1 },
     { title: "css", price: "89", id: 2, quantity: 1 },
     { title: "html", price: "79", id: 3, quantity: 1 },
     { title: "next", price: "109", id: 4, quantity: 1 },
-  ]);
+  ]);*/
+
+  const initialState = [
+    { title: "js", price: "99", id: 1, quantity: 1 },
+    { title: "css", price: "89", id: 2, quantity: 1 },
+    { title: "html", price: "79", id: 3, quantity: 1 },
+    { title: "next", price: "109", id: 4, quantity: 1 },
+  ];
+
+  const [products, dispatchProducts] = useReducer(reducer, initialState);
 
   return (
     <productsContext.Provider value={products}>
-      <productsContextDispatcher.Provider value={setProducts}>
+      <productsContextDispatcher.Provider value={dispatchProducts}>
         {children}
       </productsContextDispatcher.Provider>
     </productsContext.Provider>
@@ -23,7 +32,27 @@ const ProductsProvider = ({ children }) => {
 export default ProductsProvider;
 
 export const useProducts = () => useContext(productsContext);
-export const useProductsAction = () => {
+export const useProductsAction = () => useContext(productsContextDispatcher);
+
+const reducer = (state, action) => {
+  if (action.type === "delete")
+    return state.filter((obj) => obj.id !== action.id);
+
+  let index = state.findIndex((obj) => obj.id === action.id);
+  const product = { ...state[index] };
+  const updateProducts = [...state];
+
+  if (action.type === "add") {
+    product.quantity += action.value;
+    if (product.quantity === 0)
+      return state.filter((obj) => obj.id !== action.id);
+  } else if (action.type === "setText")
+    product.title = action.event.target.value;
+
+  updateProducts[index] = product;
+  return updateProducts;
+};
+/*export const useProductsAction = () => {
   const setProducts = useContext(productsContextDispatcher);
   const products = useProducts();
 
@@ -62,4 +91,4 @@ export const useProductsAction = () => {
   };
 
   return { deleteHandler, increaseHandler, decreseHandler, setTextHandler };
-};
+};*/
